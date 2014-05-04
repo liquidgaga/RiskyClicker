28d2a47f73397c9d5edbef30fd93dfaac10cca3f
Patrick Conaboy <patrickconaboy@gmail.com>
2014-05-03 21:56:45 -0700

    Added a shitty websocket based chatroom

--- Client/client.js
+++ Client/client.js
@@ -1,15 +1,93 @@
-var canvas = document.createElement("canvas");
-canvas.width = 500;
-canvas.height = 500;
-document.getElementsByTagName('body')[0].appendChild(canvas);
+$(function() {
+  // Canvas draw code
+	var canvas = document.createElement("canvas");
+	canvas.width = 500;
+	canvas.height = 500;
+	document.getElementsByTagName('body')[0].appendChild(canvas);
 
-var context = canvas.getContext('2d');
+	var context = canvas.getContext('2d');
+	context.fillStyle = "#0000FF";
+	context.fillRect(0,0,500,500);
+	var pic = new Image();
+	pic.onload = function()
+	{
+		context.drawImage(pic, 0, 0, 300, 300);
+	}
 
-var pic = new Image();
-pic.onload = function()
-{
-	context.drawImage(pic, 0, 0, 300, 300);
-}
+	pic.src = 'images/420praiseit.png';
 
-pic.src = 'images/420praiseit.png';
+        var socket  = io.connect('http://localhost:4004'),
+        text = $('#text');
 
+        socket.on('connect', function () {
+            text.html('connected');
+            socket.on('message', function (msg) {
+                text.html(msg);
+            });
+        });
+
+        socket.on('disconnect', function () {
+            text.html('disconnected');
+        });
+	
+  var $window = $(window);
+  var $messages = $('.messages'); // Messages area
+  var $inputMessage = $('.inputMessage'); // Input message input box
+  var $chatPage = $('.chat.page'); // The chatroom page
+
+  // Sends a chat message
+  function sendMessage () {
+    var message = $inputMessage.val();
+    // Prevent markup from being injected into the message
+    message = cleanInput(message);
+    // if there is a non-empty message
+    if (message) {
+      $inputMessage.val('');
+      addChatMessage({
+        message: message
+      });
+      // send the server a message
+      socket.emit('message', message);
+    }
+  }
+
+  // Adds the message to the browser
+  function addChatMessage (data) {
+    var messageBodyDiv = '<span class="messageBody">' +
+      data.message + '</span>';
+
+    var messageDiv = '<li class="message">' + messageBodyDiv + '</li>';
+    var $messageDiv = $(messageDiv).data('username', data.username);
+
+    addMessageElement($messageDiv);
+  }
+
+  // Adds a message element to the messages and scrolls to the bottom
+  // el - The element to add as a message
+  function addMessageElement (el) {
+    var $el = $(el);
+	$messages.append($el);
+  }
+
+  // Prevents input from having injected markup
+  function cleanInput (input) {
+    return $('<div/>').text(input).html() || input;
+  }
+
+  // Keyboard events
+
+  $window.keydown(function (event) {
+    // When the client hits ENTER on their keyboard
+    if (event.which === 13) {
+        sendMessage();
+    }
+  });
+
+  // Socket events
+
+  // Whenever the server broadcasts a message, update the chat body
+  socket.on('message', function (data) {
+    addChatMessage(data);
+  });
+
+});
--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+var
+	port        = process.env.PORT || 4004,
+	express         = require('express'),
+	http            = require('http'),
+	app             = express(),
+	server          = http.createServer(app),
+	io              = require('socket.io');
+	socket = io.listen(server);
 
-    var
-        port        = process.env.PORT || 4004,
-        io              = require('socket.io'),
-        express         = require('express'),
-        http            = require('http'),
-        app             = express(),
-        server          = http.createServer(app);
+server.listen(port)
 
-    server.listen(port)
+console.log('Listening on port ' + port );
 
-    console.log('Listening on port ' + port );
+app.use(express.static(__dirname + '/Client'));
 
-	app.use(express.static(__dirname + '/Client'));
-	
-    app.get( '/', function( req, res ){
-        console.log('Trying to load %s', __dirname + '\\..\\Client\\index.html\n');
-        res.sendfile( 'Client\\index.html' , { root: '../' });
+app.get( '/', function( req, res ){
+	console.log('Trying to load %s', __dirname + '\\..\\Client\\index.html\n');
+	res.sendfile( 'Client\\index.html' , { root: '../' });
+});
+
+// Chatroom
+// Starts listening when someone connects.
+socket.sockets.on('connection', function (socket) {
+  // when a client emits 'message', this listens and executes
+  socket.on('message', function (data) {
+    // send all clients a "message" type message. Creative, I know.
+    socket.broadcast.emit('message', {
+      message: data
     });
+  });
+});
